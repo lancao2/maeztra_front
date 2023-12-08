9b3b28a314710a8bd8d75a45be421ced3f4ec249
lancao2 <lancao.af@gmail.com>
2023-12-08 02:28:24 -0300

    aaaaaaaaaa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
     images: {
         domains: ['ibb.co'],
       },
 }
+
+module.exports = {
+  // Outras configurações...
+
+  // Adicione a configuração de exportação estática
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      // Adicione outras páginas conforme necessário
+    };
+  },
+};
 
 module.exports = nextConfig
